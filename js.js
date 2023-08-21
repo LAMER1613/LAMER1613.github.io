0143d8d42df01c37ad5f6badd3f007965dda7471
Кирилл <142844265+LAMER1613@users.noreply.github.com>
2023-08-22 02:46:51 +0300

    website files

---
+++ js.js
@@ -0,0 +1,13 @@
+// const copyLink = (e) => {
+//     e.preventDefault();
+//     let aux = document.createElement('input');
+//     let url = e.target.getAttribute('href');
+//     aux.setAttribute('value', url);
+//     document.body.appendChild(aux);
+//     aux.select();
+//     document.execCommand('copy');
+//     document.body.removeChild(aux);
+//   };
+
+// let link = document.querySelector('a');
+// link.addEventListener('click', copyLink);
